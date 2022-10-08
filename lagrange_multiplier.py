e73a448d9f74c42475849b00ea25fdf301a6d210
zhaeus <87953431+zhaeus@users.noreply.github.com>
2022-10-09 09:09:18 +1000

    Not sure if optimisation is still working or not

--- lagrange_multiplier.py
+++ lagrange_multiplier.py
@@ -1,33 +1,24 @@
 # -*- coding: utf-8 -*-
 """
 
-Script for optimising f(x1,x2,...xn) while subject to constraint g(x1,x2,...,xn) = 0
+Script for optimising f(x1,x2,...xn) while subject to constraints g_j(x1,x2,...,xn) >= 0
 
 """
 
 import numpy as np
 import matplotlib.pyplot as plt
+try:
+    import IPython
+    shell = IPython.get_ipython()
+    # To plot in Spyder window, uncomment 'inline' line:
+    # shell.enable_matplotlib(gui='inline')
+    # To plot out of Spyder window, unccoment 'qt5' line:
+    shell.enable_matplotlib(gui='qt5')
 
-
-### Troubleshooting and plotting ### 
-# def plotting():
-#     # Idiom for plotting outside of Spyder window 
-#     import matplotlib.pyplot as plt
-#     try:
-#         import IPython
-#         shell = IPython.get_ipython()
-#         shell.enable_matplotlib(gui='qt5') #qt5, inline
-#     except:
-#         print('Unable to open plotting window')
-        
-#     num_x = num_y = 20
-#     x = np.linspace(-2,2,num=num_x)
-#     y = np.linspace(-2,2,num=num_y)
-#     x,y = np.meshgrid(x,y)
-    
-#     minimise = x**3 - x**2 - 2*x - y**3 + 1      
-    
-    
+except:
+    print('Unable to open plotting window')  
+   
+## First and second vectorised partial derivative function ##
 def partial(order,function_list,function_index,x_vec,x_index):
     
     # order determines whether this is the first partial derivative order the second partial derivative
@@ -54,14 +45,44 @@ def partial(order,function_list,function_index,x_vec,x_index):
         partial_diff = (foo(x_right) - 2*foo(x_mid) + foo(x_left)) / (h_now**2)
     
     return partial_diff[0]
-    
 
-def KKT(function_list,x_init):
-    num_steps = 10
-    iteration = 0 
+def foo(x_vec):
+    x = x_vec[0]
+    y = x_vec[1]
+    return (x-1)**2 + (y-1)**4
+
+def goo(x_vec):
+    x = x_vec[0]
+    y = x_vec[1]
+    try:
+        function = 5/x**2 + 5/y**2  - 5
+    except:
+        x_vec += 1e-3
+        x = x_vec[0]
+        y = x_vec[1]
+        function = 5/x**2 + 5/y**2 -5
+    return function
+
+## Plotting ##
+fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+x = y = np.linspace(-5,5,20)
+x, y = np.meshgrid(x,y)
+z = (x-1)**2 + (y-1)**4
+ax.plot_surface(x, y, z,color='red')
+w = 5/x**2 + 5/y**2 -5
+ax.plot_surface(x,y,w,color='blue')
+# ax.axes.set_ylim3d(bottom=-5, top=5) 
+# ax.axes.set_zlim3d(bottom=-20, top=20)
+# ax.axes.set_xlim3d(bottom=-5, top=5)
+
+
+def KKT(function_list,x_init,tol):
+
     x_vec = x_init
-    while iteration<num_steps:
-        iteration += 1
+    minimise = function_list[0]
+    loop = True
+    
+    while loop==True:
         # 1. function_list is list contain function names
         # 2. First entry of function_list is the function to be minimised
         # 3. All subsequent entries in function_vec are constraints in form g_j(x1,x2,...)>=0 
@@ -69,11 +90,12 @@ def KKT(function_list,x_init):
         # 5. First column of init_vec contains initial points for numerical analysis
         # 6. Second column of init_vec contains grid distance **MAYBE CHANGE TO BE DYNAMIC**
     
-        num_constraints = len(function_list)-1  # equal to number of lambda
+        # equal to number of lambda
+        num_constraints = len(function_list) -1 
         total_num_var= np.shape(x_vec)[0] # rows of x_vec
         num_decision_vars = total_num_var - num_constraints 
-        if num_constraints > num_decision_vars:
-            raise RuntimeError('Cannot solve; too many constraints!')  
+        if num_constraints + 1 > num_decision_vars:
+            raise RuntimeError('Cannot solve - too many constraints!')  
         
         ## Evaluating function vector ##
         function_vec = np.zeros(total_num_var)
@@ -81,13 +103,14 @@ def KKT(function_list,x_init):
         for i in range(num_decision_vars):
             function_vec[i] += partial(1,function_list,0,x_vec,i) # First entry is always partial of \
                                                                   # cost function with respect to x_i
-            for j in range(1,len(function_list)):
-                function_vec[i] += -x_vec[num_decision_vars + j - 1][0] * partial(1,function_list,j,x_vec,i)
+            for j in range(num_constraints):
+                function_vec[i] += x_vec[num_decision_vars + j,0] * partial(1,function_list,j+1,x_vec,i)
             #                                                     Subsequent entries are lambda_j * 
             #                                                     partial g_j wrt x_i
-        for i in range(num_constraints):
-            constraint_foo = function_list[i+1]
-            function_vec[i + num_decision_vars] += constraint_foo(x_vec[0])
+                constraint_foo = function_list[j+1]  
+                function_vec[j + num_decision_vars] += constraint_foo(x_vec[0:num_decision_vars,0])                                   
+            
+        print(f"Function vector is {function_vec}")
         
         ## Evaluating J matrix ##
         # Saving memory for J matrix to come
@@ -102,69 +125,58 @@ def KKT(function_list,x_init):
             for i in range(num_decision_vars):
                 top_right[i,j] = partial(1,function_list,j+1,x_vec,i)
         # bottom_left rectangular matrix
-        bottom_left = -top_right.T 
+        bottom_left = top_right.T 
         bottom_right = np.zeros((num_constraints,num_constraints))
         top_matrices = np.hstack((top_left,top_right))
         bottom_matrices = np.hstack((bottom_left,bottom_right))
         J = np.vstack((top_matrices,bottom_matrices))
-        # print(np.size(J))
-        # print(np.size(x_vec[:,0]))
+        # print('Jacobian is:')
+        # print(J)
         
-        change = np.linalg.solve(J,function_vec)
-        # print(np.shape(change))
-        x_vec[:,0] -= change 
-        print(x_vec[:,0])
+        change = np.linalg.solve(J,function_vec)        
+        x_new = x_vec[:,0] - change
+        # Find some way to have an integer number of spars or whatever
+        
+        print(f"The minimal value is {minimise(x_new)}")
+        print(f"The x and y values are {x_new[0]} and {x_new[1]} respectively")
+        
+        for i in range(num_decision_vars,total_num_var):
+            if x_vec[i,0] < 0:
+                print('Redundant lambda - optimisation halted')
+                loop = False # Find some way to make this nicer
+        if np.abs(minimise(x_new)-minimise(x_vec[:,0]))/(minimise(x_vec[:,0])) < tol:
+            loop = False
+            print('Stable solution found')
         
         
-        ### very jank, please delete ###
-        if num_decision_vars == 2 and iteration == 1:
-            fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+        x_vec[:,0] = np.copy(x_new)
         
-            x = y = np.linspace(-10,10,20)
-            xx, yy = np.meshgrid(x,y)
-            
-            zz = xx**2+yy**2
-            ax.plot_surface(xx, yy, zz,color='red')
-            
-            ww = -xx-yy + 3
-            ww = ax.plot_surface(xx,yy,ww,color='blue')
-            
-            ax.set_xlim(-10,10)
-            ax.set_ylim(-10,10)
-            plt.show()
-pass
+        
+        if num_decision_vars == 2:
+            sol_x = x_vec[0,0]
+            sol_y = x_vec[1,0]
+            sol_vec = np.array((sol_x,sol_y))
+            if loop == True:
+                ax.scatter(sol_x,sol_y,minimise(sol_vec),color='black')
+            else: 
+                ax.scatter(sol_x,sol_y,minimise(sol_vec),color='green',marker='o')
+    # ax.axes.set_ylim3d(bottom=-5, top=5) 
+    ax.axes.set_zlim3d(bottom=-20, top=20)
+    # ax.axes.set_xlim3d(bottom=-5,top=5)
+    plt.show()           
+    pass
     
-    
-def foo(x_vec):
-    x = x_vec[0]
-    y = x_vec[1]
-    return x**2 + y**2
+# Analogue to defining appropriate mass and stress functions
 
-def goo(x_vec):
-    x = x_vec[0]
-    y = x_vec[1]
-    return -x -y + 3
 
 f_list = []
 f_list.append(foo)
 f_list.append(goo)
 
-x = 1*np.ones((3,2))
+# x = 1*np.ones((3,2))
+x = 2*np.ones((3,2))
 x[:,1] = 0.1
-# print(x)
-# print(partial(2,f_list,0,x,1))
-    
-KKT(f_list,x)  
-    
-    
-    
-    
-"""
 
-fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-ax.plot_surface(x,y,foo,color='blue')
 
-ax.set_title('Function surface')
-# ax.set_zlim(-lim,lim)
-# ax.set_xlim(-lim,lim)
-plt.show() """
+    
+KKT(f_list,x,tol=0.05)  
